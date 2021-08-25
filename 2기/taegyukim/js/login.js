4cef8de80c0c32f839b9229a7def12dcc1948043
이름 <ktg6361@gmail.com>
2021-08-25 15:45:31 +0900

    first-webucks-commit

---
+++ 2기/taegyukim/js/login.js
@@ -0,0 +1,17 @@
+const userInfo = document.querySelector(".userinfo");
+const user = document.querySelector(".user");
+const password = document.querySelector(".password");
+const btn = document.querySelector("#login-btn");
+
+function moveList() {
+    location.href = "/list.html";
+}
+
+function paintBtn() {
+    if(user.value.includes('@') && password.value.length >= 8) {
+        btn.style.backgroundColor = "#60ADED";
+        btn.setAttribute('onclick', 'moveList();');
+    }
+}
+
+userInfo.addEventListener('keyup', paintBtn);
