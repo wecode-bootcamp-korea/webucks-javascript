67bf7327ba140f8eba6c2c65c74beccec375212d
은정 <leecoder21@gmail.com>
2021-08-25 16:26:49 +0900

    Add: webucks page

---
+++ 2기/eunjeonglee/js/login.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const user = document.querySelector("#userId");
+const password = document.querySelector("#password");
+const loginBtn = document.querySelector("#btn");
+
+function loginColorHandler(event) {
+  const username = document.querySelector("#userId").value;
+  const userpw = document.querySelector("#password").value;
+  if((username.includes("@")) && (userpw.length >= 8)) {
+    const loginBtn = document.querySelector("#btn");
+    loginBtn.classList.add("btn");
+  } else {
+    loginBtn.classList = '';
+  }
+}
+
+window.addEventListener("keyup", loginColorHandler);
+
+const loginForm = document.querySelector("#login-form");
+
+function clickedBtn(event) {
+  event.preventDefault();
+  const username = document.querySelector("#userId").value;
+  const userpw = document.querySelector("#password").value;
+  if((username.includes("@")) && (userpw.length >= 8)) {
+  window.location.href = "http://127.0.0.1:5500/webucks/list.html"
+  }
+}
+
+loginBtn.addEventListener("click", clickedBtn)
+
+
+
+/*
+function loginClicked() {
+  if((username.includes("@")) && (userpw.length >= 8)) {
+    alert('clicked')
+  } else {
+    window.location = "#";
+  }
+}
+
+loginBtn.addEventListener("click", loginClicked)
+*/
