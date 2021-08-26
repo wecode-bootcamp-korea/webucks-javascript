84cdb62c204d01a44d29d91ef7515b13bd8ca30c
goplanit19@gmail.com <goplanit19@gmail.com>
2021-08-26 18:11:35 +0900

    Add: 리뷰 댓글 작성자, 댓글 분리하여 진행, 리뷰 입력버튼 추가, 삭제버튼 추가

--- 2기/wonguKo/listDetail.js
+++ 2기/wonguKo/listDetail.js
@@ -1,4 +1,8 @@
 const heartBtn = document.getElementById("likeBtn");
+const reviewId = document.querySelector(".inputReviewId");
+const reviewText = document.querySelector(".inputReviewText");
+const inputBtn = document.querySelector(".ReviewSubmit");
+const reviewList = document.querySelector(".reviewBox");
 let isHeart = false;
 
 heartBtn.addEventListener("click", function () {
@@ -13,22 +17,72 @@ heartBtn.addEventListener("click", function () {
   }
 });
 
-const reviewBox = document.querySelector(".inputReview");
-const reviewList = document.querySelector(".reviewBox");
+reviewId.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    reviewText.focus();
+  } else {
+    return;
+  }
+});
 
-reviewBox.addEventListener("keyup", function (event) {
-  const userReview = reviewBox.value;
+reviewText.addEventListener("keyup", function (event) {
+  const userId = reviewId.value;
+  const userReview = reviewText.value;
+
+  const userEl = document.createElement("p");
+  userEl.classList.add("reviewer");
+  userEl.innerHTML = userId;
 
   const commentEl = document.createElement("p");
   commentEl.classList.add("reviewDetail");
   commentEl.innerHTML = userReview;
 
+  const removeBtn = document.createElement("i");
+  removeBtn.className = "fas fa-trash-alt";
+
   const commentWrap = document.createElement("div");
   commentWrap.classList.add("reviewDetailWrap");
+  commentWrap.appendChild(userEl);
   commentWrap.appendChild(commentEl);
+  commentWrap.appendChild(removeBtn);
 
   if (event.key === "Enter") {
     reviewList.appendChild(commentWrap);
-    reviewBox.value = "";
+    reviewText.value = "";
+    reviewId.value = "";
+  }
+});
+
+inputBtn.addEventListener("click", function (e) {
+  const userId = reviewId.value;
+  const userReview = reviewText.value;
+
+  const userEl = document.createElement("p");
+  userEl.classList.add("reviewer");
+  userEl.innerHTML = userId;
+
+  const commentEl = document.createElement("p");
+  commentEl.classList.add("reviewDetail");
+  commentEl.innerHTML = userReview;
+
+  const removeBtn = document.createElement("i");
+  removeBtn.className = "fas fa-trash-alt";
+
+  const commentWrap = document.createElement("div");
+  commentWrap.classList.add("reviewDetailWrap");
+  commentWrap.appendChild(userEl);
+  commentWrap.appendChild(commentEl);
+  commentWrap.appendChild(removeBtn);
+
+  reviewList.appendChild(commentWrap);
+  reviewText.value = "";
+  reviewId.value = "";
+});
+
+reviewList.addEventListener("click", function (event) {
+  const reviewTarget = event.target.classList.value;
+
+  if (reviewTarget === "fas fa-trash-alt") {
+    event.target.closest(".reviewDetailWrap").remove();
   }
 });
