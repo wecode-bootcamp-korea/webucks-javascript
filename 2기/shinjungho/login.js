0f53af022363531d5538e88a0927925803a587f2
jungho shin <junghoshin@junghoui-MacBookAir.local>
2021-08-26 17:34:33 +0900

    Add: 로그인페이지 기능 구현 완료

--- 2기/shinjungho/login.js
+++ 2기/shinjungho/login.js
@@ -1,4 +1,29 @@
-function loginBox(){
-    const userId = document.getElementById('id')
-    const userPw = document.getElementById('password')
-}
+const userId = document.getElementById('id'); //id input
+const userPw = document.getElementById('password'); //pw input
+const logbtn = document.getElementsByClassName('login-btn')[0]; //login btn
+
+console.log(userId);
+
+userId.addEventListener('keyup', function () {
+    const inputId = document.getElementById('id').value;
+    const inputPw = document.getElementById('password').value;
+
+    if (inputPw.includes('@') && inputPw.length >= 8) {
+        console.log(123);
+
+        logbtn.style.opacity = 10;
+    }
+    logbtn.addEventListener("click","list.html")
+
+});
+
+
+
+
+/*
+userId.addEventListener('keyup',loginButtonColor)
+// console.log(loginButtonColor)
+userPw.addEventListener('keyup',loginButtonColor)
+logbtn.addEventListener('keyup',loginButtonColor)
+*/
+
