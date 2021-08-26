9c913cfccb6d3e43a1f879342059ea9a18338f3a
DeVeom <deveomm@gmail.com>
2021-08-27 04:06:18 +0900

    Modify : detail.html, detail.css, detail.js - detail.html : 좋아요 버튼 <a> -> <butt> 변경 - detail.css : html 변경사항에 맞춰 수정 - detail.js 	1. 좋아요이벤트 핸들러 함수 분리 (주석 참고) 	2. 좋아요 버튼, 댓글 삭제 기능 추가에 따른 댓글 추가 기능 ì 필요한 새로운 태그, 이벤트 추가
    Add : detail.html, detail.js
    - detail.html : 리뷰에 좋아요 버튼 추가
    - detail.js : 리뷰 댓글 삭ì 기능 추가가

--- 2기/junbeomham/js/detail.js
+++ 2기/junbeomham/js/detail.js
@@ -1,17 +1,21 @@
 /*
     좋아요 버튼 클릭시, 하트의 색상 변경 기능
+    새롭게 추가 되는 댓글의 좋아요 버튼에도 이벤트를 추가하기 위해 changeLikeBtnColor(btn) 메소드를 따로 정의
 */
-const likeBtn = document.querySelector(".like-wrap");
+function changeLikeBtnColor(btn) {
+    btn.addEventListener('click', function(e) {
+        this.style.color = (this.style.color === "tomato") ? "" : "tomato";
+    });
+}
+const likeBtnList = document.querySelectorAll('.like-wrap');
 
-likeBtn.addEventListener('click', function(e) {
-    this.style.color = (this.style.color === "tomato") ? "" : "tomato";
-});
+likeBtnList.forEach(el => changeLikeBtnColor(el));
 
 /*
-    리뷰로 댓글 추가하는 기능
+    리뷰 댓글 추가 기능
 */
-const reviewInput = document.querySelector(".review-input");
-const reviewList = document.querySelector(".review-list");
+const reviewInput = document.querySelector('.review-input');
+const reviewList = document.querySelector('.review-list');
 
 reviewInput.addEventListener('keyup', function(e) {
     if(e.key === "Enter" && reviewInput.value !== "") {
@@ -19,6 +23,9 @@ reviewInput.addEventListener('keyup', function(e) {
         const dlTag = document.createElement('dl');
         const dtTag = document.createElement('dt');
         const ddTag = document.createElement('dd');
+        const delBtnTag = document.createElement('button');
+        const likeBtnTag = document.createElement('button');
+        const iTag = document.createElement('i');
         
         const userId = document.cookie
             .split(';')
@@ -28,9 +35,38 @@ reviewInput.addEventListener('keyup', function(e) {
         dtTag.innerHTML = userId;
         ddTag.innerHTML = reviewInput.value;
 
+        delBtnTag.innerHTML = "삭제";
+        delBtnTag.setAttribute('class', 'del-btn');
+        addDelEvent(delBtnTag);
+
+        iTag.setAttribute('class', 'far fa-heart');
+        likeBtnTag.setAttribute('class', 'like-wrap');
+        likeBtnTag.setAttribute('id', 'review-like');
+        changeLikeBtnColor(likeBtnTag);
+
         dlTag.appendChild(dtTag);
         dlTag.appendChild(ddTag);
+        dlTag.appendChild(delBtnTag);
+        likeBtnTag.appendChild(iTag)
+        dlTag.appendChild(likeBtnTag);
         liTag.appendChild(dlTag);
         reviewList.appendChild(liTag);
+        reviewInput.value = "";
     }
-})  
+});
+
+/*
+    리뷰 댓글 삭제 기능
+    새롭게 추가된 댓글의 삭제 버튼에 이벤트를 추가하기 위해 addDelEvent(btn) 메소드를 따로 정의
+*/
+function addDelEvent(btn) {
+    btn.addEventListener('click', function(e) {
+        const review = this.parentNode.parentNode;
+        reviewList.removeChild(review);
+    });
+}
+
+const delReviewBtns = reviewList.querySelectorAll('.del-btn');
+
+//페이지 로드시 기존에 있는 댓글 버튼들에 삭제 기능 추가
+delReviewBtns.forEach(el => addDelEvent(el));
