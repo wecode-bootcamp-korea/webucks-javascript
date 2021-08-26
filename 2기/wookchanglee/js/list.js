b1da7dd837be0599660748f674907200d422a8e3
wook95 <uclee95@gmail.com>
2021-08-27 01:35:30 +0900

    Add: 반응형 구현

--- 2기/wookchanglee/js/list.js
+++ 2기/wookchanglee/js/list.js
@@ -5,14 +5,14 @@ let handleHeart = function (heart){
     if(heart.classList[0]==='far'){
       heart.classList.replace('far','fas');
       heart.style.color = "#f5425d"
-
-    //fas가 채워진 하트! 만약 하트가 채워져있다면
+      
+      //fas가 채워진 하트! 만약 하트가 채워져있다면
     }else if(heart.classList[0]==='fas'){
       heart.classList.replace('fas','far');
       heart.style.color = "#ccc"
     } 
   }
-
+  
 }
 
 const $heartsParents = document.querySelectorAll('.image-title');
@@ -20,3 +20,14 @@ const $heartsParents = document.querySelectorAll('.image-title');
 [...$heartsParents].forEach((el)=>{
   el.lastElementChild.addEventListener('click',handleHeart(el.lastElementChild));
 })
+
+
+const $toggleBtn = document.getElementById('toggle-bar');
+const $toggle = document.querySelector('.toggle');
+
+let handleToggle = function (){
+  $toggle.classList.toggle('on');  
+}
+
+
+$toggleBtn.addEventListener('click',handleToggle);
