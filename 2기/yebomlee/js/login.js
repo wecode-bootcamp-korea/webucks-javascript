f8c3795e072ec96271875365fc019c5b031fa72a
yebomlee <eebzaaa@gamil.com>
2021-08-26 15:26:40 +0900

    Refactor: self-refactoring 1 complete

--- 2기/yebomlee/js/login.js
+++ 2기/yebomlee/js/login.js
@@ -1,17 +1,14 @@
-
-
 const inputId = document.getElementById("id");
-const inputPassword = document.getElementById("password")
+const inputPassword = document.getElementById("password");
 const thisIsButton = document.getElementsByClassName("loginbutton")[0];
 
 inputPassword.addEventListener("input", function() {
   const checkId = document.getElementById("id").value;
   const checkPassword = document.getElementById("password").value;
-
   if ((checkId.includes("@")) && checkPassword.length >= 8) {
-  thisIsButton.style.opacity = 1;
-  thisIsButton.addEventListener("click", function (){
-    location.href="list.html"
+    thisIsButton.style.opacity = 1;
+    thisIsButton.addEventListener("click", function (){
+      location.href="list.html"
   })
 }
 });
