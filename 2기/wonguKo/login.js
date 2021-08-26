89c5be65c667769bc612485acbfcb696ad7a4e83
goplanit19@gmail.com <goplanit19@gmail.com>
2021-08-26 21:01:57 +0900

    Add: 로그인 id 이메일형식 일치 border 컬러 변환, password 유효성 검사 후border  컬러변환, 로그인 활성화 기능, 비밀번호 보이는 버튼 기능 활성화 구현

--- 2기/wonguKo/login.js
+++ 2기/wonguKo/login.js
@@ -1,14 +1,34 @@
 const formData = document.querySelector(".loginForm");
 const loginBtn = document.getElementById("loginBtn");
+const idBox = document.getElementById("inputId");
+const pwBox = document.getElementById("inputPw");
+const viewPwdBtn = document.getElementById("viewPasswordBtn");
 
-let isChecked = [false, false];
+let isChecked = [false, false, false];
 
 formData.addEventListener("input", (event) => {
+  // 이메일 체크
+  const emailCheck =
+    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  // 패스워드 알파벳, 숫자, 특수문자 조합 8자리 이상 체크
+  const passwordCheck =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~`!@#$%\^&*()-+=])[A-Za-z\d~`!@#$%\^&*()-+=]{8,}$/;
+
   if (event.target.type === "text") {
-    isChecked[0] = event.target.value.indexOf("@") !== -1 ? true : false;
+    if (emailCheck.test(event.target.value) !== true) {
+      idBox.style.border = "2px solid red";
+    } else {
+      idBox.style.border = "2px solid green";
+      isChecked[0] = true;
+    }
   }
   if (event.target.type === "password") {
-    isChecked[1] = event.target.value.length >= 8 ? true : false;
+    if (passwordCheck.test(event.target.value) !== true) {
+      pwBox.style.border = "2px solid red";
+    } else {
+      pwBox.style.border = "2px solid green";
+      isChecked[1] = true;
+    }
   }
   activationBtn(isChecked);
 });
@@ -24,4 +44,16 @@ const activationBtn = (isChecked) => {
 
 loginBtn.addEventListener("click", (event) => {
   document.location.href = "list.html";
+});
+
+viewPwdBtn.addEventListener("click", function (event) {
+  if (isChecked[2] === false) {
+    pwBox.setAttribute("type", "input");
+    viewPwdBtn.style.color = "black";
+    isChecked[2] = true;
+  } else {
+    pwBox.setAttribute("type", "password");
+    viewPwdBtn.style.color = "#dddddd";
+    isChecked[2] = false;
+  }
 });
