b2164c729a563e6f7c4bcd3fb6e78e44efa55dd3
wook95 <uclee95@gmail.com>
2021-08-26 19:07:50 +0900

    Add: 커피리스트의 하트 기능 추가

---
+++ 2기/wookchanglee/js/list.js
@@ -0,0 +1,22 @@
+let handleHeart = function (heart){
+  
+  return function(){
+    //far이 빈 하트! 만약 하트가 비어 있다면
+    if(heart.classList[0]==='far'){
+      heart.classList.replace('far','fas');
+      heart.style.color = "#f5425d"
+
+    //fas가 채워진 하트! 만약 하트가 채워져있다면
+    }else if(heart.classList[0]==='fas'){
+      heart.classList.replace('fas','far');
+      heart.style.color = "#ccc"
+    } 
+  }
+
+}
+
+const $heartsParents = document.querySelectorAll('.image-title');
+
+[...$heartsParents].forEach((el)=>{
+  el.lastElementChild.addEventListener('click',handleHeart(el.lastElementChild));
+})
