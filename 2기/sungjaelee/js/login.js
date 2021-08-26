808f0472eba4ef5ef5eb3cfd7fe8e4e42f0656e7
Hans Sung-jae Lee <sjhanslee@gmail.com>
2021-08-26 16:47:59 +0900

    Refactor: 코드 정리 - console.log, border, background-color 등 불필요한 것들 삭제

--- 2기/sungjaelee/js/login.js
+++ 2기/sungjaelee/js/login.js
@@ -1,46 +1,42 @@
 const loginBtn = document.querySelector(".loginBtn");
-const userID = document.querySelector("#userID");
+const userId = document.querySelector("#userId");
 const userPw = document.querySelector("#userPw");
-// const loginForm = document.querySelector(".loginFormContainer");
-// console.log("loginForm", loginForm)
+const loginForm = document.querySelector("#loginForm");
 
-console.log("userID", userID);
+console.log("loginForm", loginForm);
+console.log("loginForm.action", loginForm.action);
+console.log("loginBtn", loginBtn);
+console.log("userId", userId);
 console.log("userPw", userPw);
 console.log("userPw.value.length", userPw.value.length);
-console.log("loginBtn", loginBtn);
-// console.log(userID.value[0] === "@");
 
 let loginReady = false;
 
 function addAtSign() {
-  if (userID.value.length === 0) {
-    userID.value = "@";
-    console.log("userID", userID);
+  if (userId.value.length === 0) {
+    userId.value = "@";
+    console.log("userId", userId);
   }
 }
 
-function clearUserID() {
-  if (userID.value === "@") {
-    // console.log("works");
-    userID.value = "";
+function clearUserId() {
+  if (userId.value === "@") {
+    userId.value = "";
   }
 }
 
 function changeBtnColor() {
   //should add regexp logic in the future
-  // console.log("userPw.value.length", userPw.value.length);
   if (
-    userID.value[0] === "@" &&
-    userID.value.length >= 2 &&
+    userId.value[0] === "@" &&
+    userId.value.length >= 2 &&
     userPw.value.length >= 8
   ) {
     loginReady = true;
-    // console.log("loginReady", loginReady);
     loginBtn.style.backgroundColor = "#62ADEC";
     loginBtn.style.cursor = "pointer";
   } else {
     loginReady = false;
-    // console.log("loginReady", loginReady);
     loginBtn.style.backgroundColor = "#aed3ea";
     loginBtn.style.cursor = "default";
   }
@@ -48,13 +44,13 @@ function changeBtnColor() {
 
 function handleLogin() {
   if (loginReady) {
-    alert("SUCCESSFULLY LOGGED IN");
-    window.location.href = "./list.html";
+    loginForm.action = "./list.html";
   }
+  console.log("hello");
 }
 
-userID.addEventListener("click", addAtSign);
-userID.addEventListener("blur", clearUserID);
-userID.addEventListener("input", changeBtnColor);
+userId.addEventListener("click", addAtSign);
+userId.addEventListener("blur", clearUserId);
+userId.addEventListener("input", changeBtnColor);
 userPw.addEventListener("input", changeBtnColor);
 loginBtn.addEventListener("click", handleLogin);
