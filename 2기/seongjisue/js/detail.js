063c90f06752a299fdf3ffa8a7196f102535f47b
jisue <tjdwltn121@naver.com>
2021-08-27 21:12:26 +0900

    Correction Mission 5 and refactoring 2

--- 2기/seongjisue/js/detail.js
+++ 2기/seongjisue/js/detail.js
@@ -1,26 +1,44 @@
 
 //하트 클릭시 red로 바꾸기
-let heartIcon = document.querySelector(".heart-icon");
+let heartIcon = document.querySelector(".heartIcon");
 
 heartIcon.addEventListener('click', () => {
-    heartIcon.classList.toggle("hearbutton-js")
-
+  heartIcon.classList.toggle("hearbuttonJs");
 })
 
 
 //리뷰 박스에서 엔터 클릭시 작성되고 초기화
-let reveiwBox = document.querySelector(".reveiw-box");//input박스
-let reveiwText = reveiwBox.previousElementSibling//리뷰
-let reveiwCreate; //리뷰에서 아이디(span)
-let reveiwIdCreate; //리뷰에서 내용(p)
+let reveiwInputName = document.querySelector("#reveiwBox").children[0];//input박스
+let reveiwInputContent = document.querySelector("#reveiwBox").children[1];//input박스
+let reveiwResult = document.querySelector(".reveiwResults"); //리뷰 결과창
+let reveiwNameCreate; //리뷰에서 아이디(span)
+let reveiwContentCreate; //리뷰에서 내용(p)
 
-reveiwBox.addEventListener('keydown', (e) =>{
-    if(e.keyCode == 13){
-        reveiwCreate = document.createElement("p");
-        reveiwIdCreate = document.createElement("span");
-        reveiwCreate.prepend(reveiwIdCreate);
-        reveiwCreate.append(reveiwBox.value);
-        reveiwText.append(reveiwCreate);
-        reveiwBox.value = '';
+
+function createNewPTag() {
+  reveiwNameCreate = document.createElement("span");
+  reveiwContentCreate = document.createElement("p");
+  reveiwNameCreate.innerHTML = reveiwInputName.value;
+  reveiwNameCreate.className = 'reveiwContent';
+  reveiwContentCreate.append(reveiwNameCreate);
+  reveiwContentCreate.append(" "+reveiwInputContent.value);
+}
+
+reveiwInputContent.addEventListener('keydown', (e) => {
+  if(e.keyCode === 13){
+    if(reveiwInputName.value && reveiwInputContent.value){
+      createNewPTag();
+      reveiwResult.append(reveiwContentCreate);
+    }else if(!reveiwInputName.value){
+      alert("닉네임을 입력해주세요");
+    }else if(!reveiwInputContent.value){
+      alert("내용을 입력해주세요");
     }
+    reveiwInputName.value = null;
+    reveiwInputContent.value = null;
+  }
 })
+
+
+
+
--- 2기/seongjisue/js/login.js
+++ 2기/seongjisue/js/login.js
@@ -1,20 +1,27 @@
-const totalBox = document.getElementsByClassName("box");
+const nameBox = document.getElementsByClassName("boxTotalCSS")[0];
+const pwBox = document.getElementsByClassName("boxTotalCSS")[1];
+const buttonBox = document.getElementsByClassName("boxTotalCSS")[2];
+
+function isValialvle() {
+  return nameBox.value.includes('@') &&  pwBox.value.length > 8;
+}
+
 
 const inputBoxStyleColor = () => {
-    let firstBox = totalBox[0].value.includes('@');
-    let secondBox = totalBox[1].value.length > 8;
-    if(firstBox && secondBox){
-        totalBox[2].style.opacity = '1';
-        //totalBox[2].click = () => {window.open('list.html')}
-        totalBox[2].addEventListener('click', () => {
-            window.open('list.html')
-        })
-    }else {
-        totalBox[2].style.opacity = '0.2';
-    }
+  console.log(isValialvle())
+  if(isValialvle()){
+    buttonBox.style.opacity = 1;
+    //totalBox[2].click = () => {window.open('list.html')}
+    buttonBox.addEventListener('click', () => {
+      window.open('list.html');
+    })
+  }else {
+    buttonBox.style.opacity = 0.5;
+  }
 }
-totalBox[0].addEventListener('keydown', inputBoxStyleColor);
-totalBox[1].addEventListener('keydown', inputBoxStyleColor);
+
+nameBox.addEventListener('keydown', inputBoxStyleColor);
+pwBox.addEventListener('keydown', inputBoxStyleColor);
 
 
 
