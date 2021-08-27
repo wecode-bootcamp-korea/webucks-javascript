0f86f6103e542f0160b53a56e450095598dd96eb
한지훈 <jihoonkr821@gmail.com>
2021-08-27 23:48:04 +0900

    5단계 완료

--- 2기/jihoonhan/Webucks/js/detail.js
+++ 2기/jihoonhan/Webucks/js/detail.js
@@ -1,5 +1,7 @@
-const heart = document.querySelector('.heart');
 const emptyHeart = document.querySelector('.far');
+const comment = document.querySelector('.comment');
+const newComment = document.querySelector('.newComment');
+const comments = document.querySelector('.comments');
 
 let onoff = false;
 
@@ -14,4 +16,18 @@ function joayo() {
   }
 }
 
+function addComment (event) {
+  if(event.keyCode==13) {
+    const newDiv = document.createElement('div');
+    const newSpan = document.createElement('span');
+    newDiv.classList.add('commentRow');
+    newSpan.classList.add('comment');
+    newSpan.innerText = newComment.value;
+    newDiv.appendChild(newSpan);
+    comments.appendChild(newDiv);
+    newComment.value = "";
+  }
+}
+
 emptyHeart.addEventListener('click', joayo);
+newComment.addEventListener('keydown', addComment);
