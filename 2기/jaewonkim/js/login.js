2c62d5ddfcf1ef474d33d6133915314a0807175e
jambottle <jaewon.walks@gmail.com>
2021-08-27 18:12:19 +0900

    Refactor: 1차 리팩토링 진행 완료 - login.css에서 불필요한 스타일 속성 제거 - login.js에서 핸들러 함수 속 스타일 속성 변경 코드를 CSS로 이전 - login.js에서 핸들러 함수 간소화 (if문 속 조건을 외부 변수로 지정) - list.html에서 '제주 비자림 콜드 브루' 이미지에 페이지 이동 링크 구현

--- 2기/jaewonkim/js/login.js
+++ 2기/jaewonkim/js/login.js
@@ -3,15 +3,18 @@ const inputUserName = document.getElementsByClassName('form-input')[0];
 const inputPassword = document.getElementsByClassName('form-input')[1];
 const inputButton = document.getElementsByClassName('form-input')[2];
 
-const verifyForm = function () {
-  if (inputUserName.value.includes('@') && inputPassword.value.length >= 8) {
+const isValidInput = () => {
+  return inputUserName.value.includes('@') && inputPassword.value.length >= 8;
+};
+
+const verifyForm = () => {
+  if (isValidInput()) {
     inputButton.disabled = false;
-    inputButton.style.opacity = '1.0';
+    inputButton.classList.add('active');
     inputForm.action = 'list.html';
   } else {
     inputButton.disabled = true;
-    inputButton.style.opacity = '0.6';
-    inputForm.action = 'list.html';
+    inputButton.classList.remove('active');
   }
 };
 
