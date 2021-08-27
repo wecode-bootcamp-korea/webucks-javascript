d34ac062deb4907c9184d5493b772311436429c6
wonyoung <ablack1029@gmail.com>
2021-08-27 16:32:11 +0900

    Update login.js

--- 2기/wonyoungkim/js/login.js
+++ 2기/wonyoungkim/js/login.js
@@ -4,40 +4,21 @@ const btn = document.querySelector('#loginBtn');
 
 
 
-const Password = (pwd) => {
-    let letters = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/;
-    if (letters.test(pwd)) {
-        return true;
-    } else {
-        return false;
-    }
+const inValidInput = () => {
+  return (user.value.includes("@") && pwd.value.length >= 8)
 }
 
-const Email = (user) => {
-  let letters = /^([a-z0-9_]){8,}$/;
-  if (letters.test(user)) {
-      return true;
-  } else {
-      return false;
+const changeColor =  () => {
+  const isValid = inValidInput()
+  if (isValid) {
+    btn.style.backgroundColor = "#398dd6";
+  }else {
+    btn.style.backgroundColor = "#C4E1FB";
   }
-}
+};
 
-const disButton = () => {
-    btn.setAttribute("disabled")
-    btn.style.backgroundColor = "#add3ea";
-  }
-
-const Button = () => {
-    btn.removeAttribute("disabled")
-    btn.style.backgroundColor = "#72bd2c";
-  }
-
-
-document.addEventListener('input', () => {
-    if(Email(user.value) && Password(pwd.value)) {
-        disButton();
-    }
-})
+user.addEventListener('input', changeColor)
+pwd.addEventListener('input',changeColor)
 
 
 
