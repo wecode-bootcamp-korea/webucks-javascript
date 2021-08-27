21a798a00585518cb41a4b21e6dab799a03171af
han0gu <imgum9@gmail.com>
2021-08-27 10:38:49 +0900

    Modify: change indentation space 4 to 2

--- 2기/swhan/js/coffeeList.js
+++ 2기/swhan/js/coffeeList.js
@@ -2,8 +2,8 @@ const coffeeImgdivList = document.querySelectorAll('.grid-coffee div');
 const coffeeImgList = document.querySelectorAll('.grid-coffee img');
 
 const len = coffeeImgList.length;
-for (let idx=0; idx < len; idx++) {
-    const pTag = document.createElement('p');
-    pTag.textContent = coffeeImgList[idx].alt;
-    coffeeImgdivList[idx].appendChild(pTag);
+for (let idx = 0; idx < len; idx++) {
+  const pTag = document.createElement('p');
+  pTag.textContent = coffeeImgList[idx].alt;
+  coffeeImgdivList[idx].appendChild(pTag);
 }
--- 2기/swhan/js/index.js
+++ 2기/swhan/js/index.js
@@ -6,16 +6,16 @@ const btnLogin = document.getElementById('btn-login'); // login 버튼
  * @param {*} _event keyup event
  * @description check id-pw validation and change CSS on login button
  */
-function checkValidation (_event) {
-    const strId = iptId.value;
-    const strPw = iptPw.value;
-    if (strId.indexOf('@') > -1 && strPw.length >= 8) {
-        btnLogin.style.backgroundColor = '#60ADED';
-        btnLogin.disabled = false;
-    } else {
-        btnLogin.style.backgroundColor = '#ADD3EA';
-        btnLogin.disabled = true;
-    }
+function checkValidation(_event) {
+  const strId = iptId.value;
+  const strPw = iptPw.value;
+  if (strId.indexOf('@') > -1 && strPw.length >= 8) {
+    btnLogin.style.backgroundColor = '#60ADED';
+    btnLogin.disabled = false;
+  } else {
+    btnLogin.style.backgroundColor = '#ADD3EA';
+    btnLogin.disabled = true;
+  }
 }
 
 iptId.addEventListener('keyup', checkValidation); // id 입력란 이벤트 bind
