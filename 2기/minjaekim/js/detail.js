31629264879ab133e9593451226f0037e412e419
minjaekim <minjae2246@gmail.com>
2021-08-28 19:46:01 +0900

    Add 5차 미션 완료

--- 2기/minjaekim/js/detail.js
+++ 2기/minjaekim/js/detail.js
@@ -1,33 +1,30 @@
+// 하트 채우기 이벤트
 const heartIcon = document.getElementById('heartIcon');
 
 const fillHeartIcon = () => {
-  if(heartIcon.style.color === 'tomato'){
-    return heartIcon.style.color = 'gray'
-  } else {
-    return heartIcon.style.color = 'tomato'
-  }
+  heartIcon.classList.toggle('fillHeartIconColor')
 }
+
 heartIcon.addEventListener("click", fillHeartIcon);
 
-const reviewInputBox = document.getElementById('reviewInputBox');
-const reviewStatus =  document.getElementById('reviewStatus');
-const reviewList = document.createElement('li')
-const userId = document.createElement('span');
-const reviewContents = document.createElement('span');
-reviewList.classList.add('reviewList');
-userId.classList.add('userId');
-reviewContents.classList.add('reviewContents');
+// 리뷰달기 이벤트
 
 const sendReview = (event) => {
-  const userReviewContents = reviewInputBox.value;
-  if (event.code==='Enter') {
+  let userReviewContents = event.target.value;
+  const reviewStatus =  document.getElementById('reviewStatus');
+  const reviewList = document.createElement('li')
+  const userId = document.createElement('span');
+  const reviewContents = document.createElement('span');
+
+  if (event.key==='Enter') {
     event.preventDefault();
     reviewList.prepend(userId);
-    reviewList.appendChild(reviewContents);
     reviewContents.innerText = userReviewContents;
-    return reviewStatus.appendChild(reviewList);
-  }
+    reviewList.appendChild(reviewContents);
+    reviewStatus.appendChild(reviewList);
+    return reviewInputBox.value = ""
+  } 
 }
-reviewInputBox.addEventListener('keypress', sendReview)
+reviewInputBox.addEventListener('keydown', sendReview)
 
 
