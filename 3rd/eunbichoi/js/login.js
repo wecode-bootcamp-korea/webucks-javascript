539065fae578cb774a2d6022b96d69d980fa3f1b
ebchoi <eunbichoi1@gmail.com>
2021-11-24 17:05:18 +0900

    Add: Login page
    - create markup for login page
    - set layout and style for login form
    - add event to input forms to validate login button (change opacity upon
      validation)
    - disabled button default submit button to be enabled upon login
      validation
    - login button redirected to list page on click

---
+++ 3rd/eunbichoi/js/login.js
@@ -0,0 +1,47 @@
+let $username = document.querySelector('#username');
+let $password = document.querySelector('#password');
+let $loginButton = document.getElementById('login-button');
+
+
+const isValidLogin = [false, false]
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  $username.addEventListener('blur', () => {
+    isValidLogin[0] = ($username.value.includes('@')) ? true : false
+
+    validateLogin()
+  })
+
+  $password.addEventListener('blur', () => {
+    isValidLogin[1] = ($password.value.length > 7) ? true : false
+    
+    validateLogin()
+  })
+
+  $loginButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href = 'list.html';
+  })
+
+});
+
+
+// function validateUsername(username) {
+//   return (username.includes('@'))? true : false
+// }
+
+// function validatePassword(password) {
+//   return (password.length > 7)? true : false
+// }
+
+function validateLogin() {
+  if (isValidLogin[0] && isValidLogin[1]){
+    $loginButton.classList.add('login-validated')
+    $loginButton.removeAttribute('disabled')
+  } 
+  else {
+    $loginButton.classList.remove('login-validated')
+    $loginButton.setAttribute('disabled', true)
+  }
+}
