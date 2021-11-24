81ee360a4eae1e1ce7d2e77aaf95705e7b56c16d
minsukang <minsu910725@gmail.com>
2021-11-24 17:31:06 +0900

    minsuKang 위벅스 1차 리팩토링 및 수정 완료

--- 3rd/minsukang/js/login.js
+++ 3rd/minsukang/js/login.js
@@ -20,29 +20,16 @@ PW.addEventListener("input", function () {
     let ID = document.getElementById("id");
     let PW = document.getElementById("pw");
     let login = document.getElementById("loginbt");
+        
     if (ID.value.includes("@") && PW.value.length>=8) {
-        login.removeAttribute("disabled");
-        login.style.backgroundColor = "#60ADED";
-        login.style.cursor = "pointer";
-    } else if(ID.value.includes("@") === false || PW.value.length<8){
+            login.removeAttribute("disabled");
+            login.style.backgroundColor = "#60ADED";
+            login.style.cursor = "pointer";
+        } 
+    else if(ID.value.includes("@") === false || PW.value.length<8){
         login.setAttribute("disabled", "");
         login.style.backgroundColor = "#add3eb";
         login.style.cursor = "default";
     }
   });
 
-//   const button = document.getElementsByClassName('login')[0];
-//   const input = document.getElementsByClassName('button1')[0];
-//   const name = document.getElementsByClassName("id")[0];
-//   const pw = document.getElementsByClassName("pw")[0];
-  
-//   function changeColor() {
-//     let idValue = loginID.value;
-//     let pwValue = loginPW.value;
-//     if (idValue.includes('@') && pwValue.length>=8) {
-//           login.removeAttribute('disabled');
-//           login.style.backgroundColor='#0095F6';
-//       }
-//   }
-  
-//   input.addEventListener('keyup',changeColor);
