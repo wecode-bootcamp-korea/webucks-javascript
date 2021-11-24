35d0960930bd8a783aa56fb71b4a770a7692d215
JongminChoi <quark4904@gmail.com>
2021-11-24 15:36:41 +0900

    initial commit

---
+++ 3rd/jongminchoi/webucks/js/login.js
@@ -0,0 +1,27 @@
+let id = document.getElementById("emailInput");
+let pw = document.getElementById("passwordInput");
+let thisIsButton = document.getElementById("login-button");
+
+id.addEventListener("input", function () {
+  if (id.value.includes("@") && pw.value.length >= 8) {
+    thisIsButton.removeAttribute("disabled");
+    thisIsButton.style.backgroundColor = "#60ADED";
+    thisIsButton.style.cursor = "pointer";
+  } else if (pw.value.length < 8 || id.value.includes("@") === false) {
+    thisIsButton.setAttribute("disabled", "");
+    thisIsButton.style.backgroundColor = "#add3eb";
+    thisIsButton.style.cursor = "default";
+  }
+});
+
+pw.addEventListener("input", function () {
+  if (id.value.includes("@") && pw.value.length >= 8) {
+    thisIsButton.removeAttribute("disabled");
+    thisIsButton.style.backgroundColor = "#60ADED";
+    thisIsButton.style.cursor = "pointer";
+  } else if (pw.value.length < 8 || id.value.includes("@") === false) {
+    thisIsButton.setAttribute("disabled", "");
+    thisIsButton.style.backgroundColor = "#add3eb";
+    thisIsButton.style.cursor = "default";
+  }
+});
