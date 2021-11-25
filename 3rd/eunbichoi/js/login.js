e914732858ed8a73e6c86a5e240df25d4af74089
ebchoi <eunbichoi1@gmail.com>
2021-11-26 04:09:46 +0900

    Add: extra login features
    - create feature to change border color of input upon validation
    - add button to show/hide password

--- 3rd/eunbichoi/js/login.js
+++ 3rd/eunbichoi/js/login.js
@@ -1,6 +1,7 @@
 let $username = document.querySelector('#username');
 let $password = document.querySelector('#password');
 let $loginButton = document.getElementById('login-button');
+let $passwordButton = document.querySelector('.password-button');
 
 const isValidLogin = [false, false]
 
@@ -8,19 +9,46 @@ window.addEventListener('DOMContentLoaded', () => {
 
   $username.addEventListener('blur', () => {
     isValidLogin[0] = ($username.value.includes('@')) ? true : false
-
+    
+    if (isValidLogin[0]) {
+      $username.parentNode.classList.add('validated');
+    }
+    else if (!isValidLogin[0]) {
+      $username.parentNode.classList.remove('validated');
+    }
     validateLogin()
   })
 
   $password.addEventListener('blur', () => {
     isValidLogin[1] = ($password.value.length > 7) ? true : false
     
+    if (isValidLogin[1]) {
+      $password.parentNode.classList.add('validated');
+    }
+    else if (!isValidLogin[1]) {
+      $password.parentNode.classList.remove('validated');
+    }
     validateLogin()
   })
 
   $loginButton.addEventListener('click', (e) => {
     e.preventDefault();
     location.href = 'list.html';
+  })
+  
+  $passwordButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log($passwordButton.textContent)
+    if ($passwordButton.textContent === 'show') {
+      $password.removeAttribute('type');
+      $password.setAttribute('type', 'text');
+      $passwordButton.textContent="hide";
+    }
+    else if ($passwordButton.textContent === 'hide') {
+      $password.removeAttribute('type');
+      $password.setAttribute('type', 'password');
+      $passwordButton.textContent="show"
+    }
   })
 
 });
