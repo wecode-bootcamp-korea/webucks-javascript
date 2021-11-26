986db6fe1b3b63696aaad8540e9d552311845082
guminki <rnalsrl93@gmail.com>
2021-11-26 21:47:39 +0900

    Modify: 상세페이지 반응형 적용

--- 3rd/js/detail.js
+++ 3rd/js/detail.js
@@ -4,6 +4,7 @@ let inputBox = document.getElementById("writeReview");
 // 리뷰 남겨질 section 변수 선언
 let reviews = document.getElementById("reviews");
 
+// 리뷰 process
 inputBox.addEventListener("keyup", function(e) {
     if (e.code === "Enter") {
         let written = document.createElement('p');
@@ -13,11 +14,11 @@ inputBox.addEventListener("keyup", function(e) {
     }
 }
 )
-
+// 테두리 하트아이콘 태그
 let heartouter = document.getElementsByClassName("far")[0];
-
+// 속이찬 하트 아이콘 태그
 let heartinner = document.getElementsByClassName("fas")[0];
-
+// 클릭시 하트아이콘 색 변화
 heartinner.addEventListener("click", function() {
     if (heartinner.style.opacity == 0) {
         heartinner.style.opacity="0.4";
--- 3rd/js/login.js
+++ 3rd/js/login.js
@@ -1,17 +1,19 @@
-// 로그인 버튼 활성화 조건 설정
+// iput 태그에 접근
 let input = document.getElementsByClassName('innerBox');
 
+// button 태그에 접근
 const button = document.getElementById('button');
 
-// 버튼 활성화 함수
+// 버튼 활성화 기능 함수
 function buttonActiv() {
-    const id = document.getElementById('id').value;
-    const password = document.getElementById('password').value;
+    const id = document.getElementById('id').value; //이메일 input란에 입력되는 값
+    const password = document.getElementById('password').value; // pw input란에 입력되는 값
     const button = document.getElementById('button');
     if (id.includes('@') && password.length >= 8) {
         button.style="cursor:pointer";
         button.disabled = false;
     } else {
+        // 조건이 다시 불만족 되었을 시에 다시 비활성화 기능
         button.style="cursor:defalt"
         button.disabled = true;
     }
@@ -22,6 +24,8 @@ input[0].addEventListener('keyup', buttonActiv);
 // 비밀번호란에 입력 발생시
 input[1].addEventListener('keyup', buttonActiv);
 
+
+// button 클릭시 디테일 페이지로 이동하는 기능
 button.addEventListener('click', function() {
     window.location.href = 'list.html'
 })
