81e4602d44b8ea1c1d7c2f6b84d906e8434826c4
heeyun <ato8757@gmail.com>
2021-11-26 11:01:25 +0900

    하트 색깔 바꾸는 기능 추가

--- 3rd/heeyun/detail.js
+++ 3rd/heeyun/detail.js
@@ -1,24 +1,52 @@
-const elID = document.getElementById('ID');
-const elPassword = document.getElementById('password');
-const valID = elID.value;
-const valPassword = elPassword.value;
+// const elID = document.getElementById('ID');
+// const elPassword = document.getElementById('password');
+// const valID = elID.value;
+// const valPassword = elPassword.value;
 
-function checkConditions(){
+// function checkConditions(){
 
-}
+// }
 
-elPassword.addEventListener('keyup',function(){
-    console.log(valID);
-    console.log(valPassword);
-    if(valID.includes('@') && valPassword.length>7){
+// elPassword.addEventListener('keyup',function(){
+//     console.log(valID);
+//     console.log(valPassword);
+//     if(valID.includes('@') && valPassword.length>7){
         
-        assignment('yes');
-    }
-});
+//         assignment('yes');
+//     }
+// });
 
-function assignment(word){
-    console.log(word);
-    if(word==='yes'){
-        document.querySelector("button").style.backgroundColor='#60ADED';
-    }
+// function assignment(word){
+//     console.log(word);
+//     if(word==='yes'){
+//         document.querySelector("button").style.backgroundColor='#60ADED';
+//     }
+// }
+
+const btn = document.getElementById('heart');
+// function changeHeartColor(){
+//     if(btn.color === 'black'){
+//         btn.addEventListener('click',function(){
+//     btn.style.color='red';  
+//         })
+//     }else{
+//         btn.addEventListener('click',function(){
+//             btn.style.color='black';  
+//         })
+//     }
+// }
+// changeHeartColor();
+count = 0;
+btn.style.color = 'black';
+function countChange(){
+    btn.addEventListener('click',function(){
+        count++;
+        if(count%2===0){
+            btn.style.color = 'black'
+        }
+        if(count%2===1){
+            btn.style.color = 'red'
+        }
+    })
 }
+countChange();
