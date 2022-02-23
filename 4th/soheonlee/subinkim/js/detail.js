8bf71cd85eb8502a79cc4cb1b7a22863e5607e91
김수빈 <ksb334ksb@gmail.com>
2022-02-23 23:39:15 +0900

    Add: review add

--- 4th/soheonlee/subinkim/js/detail.js
+++ 4th/soheonlee/subinkim/js/detail.js
@@ -1,11 +1,43 @@
 const heart = document.getElementsByTagName('i')[0];
+const reviewInput = document.getElementsByClassName('reviewInput')[0];
 
-heart.addEventListener('click' , function() {
+function heartOnOff () {
     if (heart.classList.contains('fa-regular')) {
         heart.classList.replace('fa-regular', 'fa-solid');
         heart.classList.replace('btnOff', 'btnOn');
     } else {
         heart.classList.replace('fa-solid', 'fa-regular');
         heart.classList.replace('btnOn', 'btnOff');
+    }
+};
+
+function submit () {
+    const reviewBox = document.getElementsByClassName('reviewContents')[0];
+    
+    const reviewTool = document.createElement('div')
+    reviewTool.classList.add('aReview');
+    
+    const reviewName = document.createElement('span');
+    
+    const reviewText = document.createElement('p');
+    reviewText.classList.add('aReivewText')
+    
+    reviewName.innerHTML = prompt('아아디를 입력해주세요.', 'customer');
+    reviewText.innerHTML = reviewInput.value;
+
+    reviewTool.appendChild(reviewName);
+    reviewTool.appendChild(reviewText);
+    
+    reviewBox.appendChild(reviewTool);
+};
+
+
+
+heart.addEventListener('click', () => heartOnOff() );
+
+reviewInput.addEventListener('keyup', (e) => {
+    if(reviewInput.value && e.key == 'Enter') {
+        submit();
+        reviewInput.value = '';
     }
 });
--- 4th/soheonlee/subinkim/js/login.js
+++ 4th/soheonlee/subinkim/js/login.js
@@ -3,13 +3,6 @@ const loginID = document.getElementsByClassName('loginInput')[0];
 const loginPW = document.getElementsByClassName('loginInput')[1];
 const loginButton = document.getElementsByClassName('loginButton')[0];
 
-// 로그인 활성화 & 비활성화 이벤트
-loginForm.addEventListener('keyup', function() {
-    loginID.value.includes('@') && loginPW.value.length >= 8 
-    ? setLoginBtn(false, '#62adec') 
-    : setLoginBtn(true, '#d5e7f8');
-});
-
 // 로그인 버튼 활성화 함수
 function setLoginBtn(isAble, btnColor) {
     loginButton.disabled = isAble;
@@ -21,8 +14,16 @@ function toList() {
     window.location.href = "list.html";
 }
 
+
+// 로그인 활성화 & 비활성화 이벤트
+loginForm.addEventListener('keyup', () => {
+    loginID.value.includes('@') && loginPW.value.length >= 8 
+    ? setLoginBtn(false, '#62adec') 
+    : setLoginBtn(true, '#d5e7f8');
+});
+
 // ENTER키 누르면 화면 넘어가는 이벤트
-window.addEventListener('keyup', function(e) {
+window.addEventListener('keyup', (e) => {
     if(e.key === 'Enter' && !loginButton.disabled) {
         toList();        
     }
