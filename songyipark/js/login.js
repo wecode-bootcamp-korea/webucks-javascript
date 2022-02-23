6feaa309f0fa1db751cb8dda7bb879f1789cacc2
songyi-p <songyi7091@gmail.com>
2022-02-23 20:16:26 +0900

    [Refactor] : Mission 1~3 리팩토링 완료

--- songyipark/js/login.js
+++ songyipark/js/login.js
@@ -1,15 +1,15 @@
-const inputId = document.querySelector('.id')
-const inputPw = document.querySelector('.pw')
-const disabledBtn = document.querySelector('.login-btn')
+const inputId = document.querySelector(".id");
+const inputPw = document.querySelector(".pw");
+const disabledBtn = document.querySelector(".login-btn");
 
-inputPw.addEventListener("keyup", activateBtn)
+inputPw.addEventListener("keyup", activateBtn);
 
 function activateBtn() {
-    if (inputId.value.includes("@") && inputPw.value.length >= 8) {
+  if (inputId.value.includes("@") && inputPw.value.length >= 8) {
     disabledBtn.disabled = false;
     disabledBtn.style.opacity = 1;
-    } else {
+  } else {
     disabledBtn.disabled = true;
     disabledBtn.style.opacity = 0.4;
-}
+  }
 }
