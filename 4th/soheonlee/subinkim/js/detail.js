84425199657ae7a27711ba3294c868edf88a3e9e
김수빈 <ksb334ksb@gmail.com>
2022-02-24 16:36:48 +0900

    Modify: primary refactoring

--- 4th/soheonlee/subinkim/js/detail.js
+++ 4th/soheonlee/subinkim/js/detail.js
@@ -1,26 +1,31 @@
 const heart = document.getElementsByTagName('i')[0];
-const reviewInput = document.getElementsByClassName('reviewInput')[0];
+const reviewInput = document.getElementsByClassName('rvInput')[0];
+
+function classChange (target, current, change) {
+    target.classList.replace(current, change)
+};
 
 function heartOnOff () {
     if (heart.classList.contains('fa-regular')) {
-        heart.classList.replace('fa-regular', 'fa-solid');
-        heart.classList.replace('btnOff', 'btnOn');
+        classChange(heart, 'fa-regular', 'fa-solid');
+        classChange(heart, 'btnOff', 'btnOn');
     } else {
-        heart.classList.replace('fa-solid', 'fa-regular');
-        heart.classList.replace('btnOn', 'btnOff');
+        classChange(heart, 'fa-solid', 'fa-regular');
+        classChange(heart, 'btnOn', 'btnOff');
     }
 };
 
 function submit () {
-    const reviewBox = document.getElementsByClassName('reviewContents')[0];
+    const reviewBox = document.getElementsByClassName('rvContents')[0];
     
     const reviewTool = document.createElement('div')
-    reviewTool.classList.add('aReview');
+    reviewTool.classList.add('aRv');
     
     const reviewName = document.createElement('span');
+    reviewName.classList.add('rvId')
     
     const reviewText = document.createElement('p');
-    reviewText.classList.add('aReivewText')
+    reviewText.classList.add('rvText')
     
     reviewName.innerHTML = prompt('아아디를 입력해주세요.', 'customer');
     reviewText.innerHTML = reviewInput.value;
