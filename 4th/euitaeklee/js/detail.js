afcac933f525e9456b42064197bdb889a41995f3
eui <wlsun0324@gmail.com>
2022-02-24 20:24:17 +0900

    Add:상세페이지구현

--- 4th/euitaeklee/js/detail.js
+++ 4th/euitaeklee/js/detail.js
@@ -2,12 +2,29 @@ window.onload = function() {
 
     let parameterSplit = location.href.split("?")[1].split("&");
 
-    parameterSplit = decodeURI(decodeURIComponent(parameterSplit));
+    function objectParameter(parameter) {
+        parameter = parameterSplit.reduce((parameter, cursor) => {
+            parameter[cursor.split("=")[0]] = decodeURI(decodeURIComponent(cursor.split("=")[1]));
+            return parameter;
+        }, {});
+        return parameter;
+    }
 
-    //parameterSplit = parameterSplit.replace(",","");
+    function valueInput() {
 
-    //parameterSplit = parameterSplit.split("=");
+        parameterSplit = objectParameter(parameterSplit);
+        
+        let targetTitle = document.querySelectorAll(".coffee_type h3")[0];
+        let targetImg = document.querySelectorAll(".coffee_detail_container .coffee_detail_img img")[0];
+        let targetProductName = document.querySelectorAll(".coffee_detail_container .coffee_detail h4")[0];
+        let targetDeatailAllergy = document.querySelectorAll(".coffee_detail_container .coffee_detail .coffee_detail_allergy p")[0];
 
-    console.log(parameterSplit);
+        targetTitle.textContent = parameterSplit.type;
+        targetImg.setAttribute("src", parameterSplit.imgUrl);
+        targetProductName.textContent = parameterSplit.productName;
+        targetDeatailAllergy.textContent = "알레르기 유발요인 : " + parameterSplit.allergy;
 
+    }
+
+    valueInput();
 }
