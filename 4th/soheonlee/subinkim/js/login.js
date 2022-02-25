d519624e9175683ae2487710eb1a5f9c9457d79b
김수빈 <ksb334ksb@gmail.com>
2022-02-26 01:26:05 +0900

    Modify: primary refactoring & Add: mission 6 done

--- 4th/soheonlee/subinkim/js/login.js
+++ 4th/soheonlee/subinkim/js/login.js
@@ -1,7 +1,8 @@
 const loginForm = document.getElementsByClassName('loginForm')[0];
 const loginID = document.getElementsByClassName('loginInput')[0];
 const loginPW = document.getElementsByClassName('loginInput')[1];
-const loginButton = document.getElementsByClassName('loginButton')[0];
+const loginButton = document.getElementsByClassName('loginBtn')[0];
+const eye = document.getElementsByClassName('eye')[0];
 
 // 로그인 버튼 활성화 함수
 function setLoginBtn(isAble, btnColor) {
@@ -14,11 +15,33 @@ function toList() {
     window.location.href = "list.html";
 }
 
+// id 활성화 이벤트
+loginID.addEventListener('keyup', () => {
+    if (loginID.value.includes('@')) {
+     loginID.classList.add('active')
+     loginID.classList.remove('inActive')
+    } else {
+     loginID.classList.add('inActive')
+     loginID.classList.remove('active')
+    } 
+});
 
-// 로그인 활성화 & 비활성화 이벤트
+// pw 활성화 이벤트
+loginPW.addEventListener('keyup', () => {
+    const required = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+    if (required.test(loginPW.value)) {
+     loginPW.classList.add('active')
+     loginPW.classList.remove('inActive')
+    } else {
+     loginPW.classList.add('inActive')
+     loginPW.classList.remove('active')
+    } 
+});
+
+// button 활성화 이벤트
 loginForm.addEventListener('keyup', () => {
-    loginID.value.includes('@') && loginPW.value.length >= 8 
-    ? setLoginBtn(false, '#62adec') 
+    loginID.classList.contains('active') && loginPW.classList.contains('active')
+    ? setLoginBtn(false, '#62adec')
     : setLoginBtn(true, '#d5e7f8');
 });
 
@@ -28,4 +51,16 @@ window.addEventListener('keyup', (e) => {
         toList();        
     }
 });
+
+// pw show / hide 이벤트
+eye.addEventListener('click', () => {
+    if (eye.classList.contains('fa-eye-slash')) {
+        eye.classList.replace('fa-eye-slash', 'fa-eye');
+        loginPW.type = 'text'
+    } else {
+        eye.classList.replace('fa-eye', 'fa-eye-slash');
+        loginPW.type = 'password'
+    }
+})
+
 
