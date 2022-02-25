6d4dda354ac475bbc34f88b5986348cdf3176d6c
김수빈 <ksb334ksb@gmail.com>
2022-02-25 18:31:52 +0900

    Modify: primary refactoring & Add: review toggle

--- 4th/soheonlee/subinkim/js/detail.js
+++ 4th/soheonlee/subinkim/js/detail.js
@@ -1,19 +1,26 @@
-const heart = document.getElementsByTagName('i')[0];
+const hearts = document.querySelectorAll('i.fa-heart');
+const trashes = document.querySelectorAll('i.fa-trash-can');
 const reviewInput = document.getElementsByClassName('rvInput')[0];
 
-function classChange (target, current, change) {
-    target.classList.replace(current, change)
-};
+// heart on / off
+hearts.forEach(heart => {
+     heart.addEventListener('click', () => {
+        if(heart.classList.contains("fa-regular")) {
+            heart.className = "fa-solid fa-heart btnOn";
+        } else {
+            heart.className = "fa-regular fa-heart btnOff";
+        }
+    })
+});
 
-function heartOnOff () {
-    if (heart.classList.contains('fa-regular')) {
-        classChange(heart, 'fa-regular', 'fa-solid');
-        classChange(heart, 'btnOff', 'btnOn');
-    } else {
-        classChange(heart, 'fa-solid', 'fa-regular');
-        classChange(heart, 'btnOn', 'btnOff');
-    }
-};
+// review remove
+trashes.forEach(trash => {
+    trash.addEventListener('click', () => {
+        trash.parentElement.remove();
+    })
+})
+
+
 
 function submit () {
     const reviewBox = document.getElementsByClassName('rvContents')[0];
@@ -24,21 +31,38 @@ function submit () {
     const reviewName = document.createElement('span');
     reviewName.classList.add('rvId')
     
-    const reviewText = document.createElement('p');
-    reviewText.classList.add('rvText')
+    const reviewText = document.createElement('span');
+    reviewText.classList.add('rvText', 'bugFix')
+
+    const heartIcon = document.createElement('i');
+    heartIcon.classList.add('fa-regular', 'fa-heart', 'btnOff', 'bugFix');
+
+    const trashIcon = document.createElement('i');
+    trashIcon.classList.add('fa-solid', 'fa-trash-can');
+
     
     reviewName.innerHTML = prompt('아아디를 입력해주세요.', 'customer');
     reviewText.innerHTML = reviewInput.value;
 
     reviewTool.appendChild(reviewName);
     reviewTool.appendChild(reviewText);
+    reviewTool.appendChild(heartIcon);
+    reviewTool.appendChild(trashIcon);
     
     reviewBox.appendChild(reviewTool);
+
+    heartIcon.addEventListener('click', () => {
+        if(heartIcon.classList.contains("fa-regular")) {
+            heartIcon.className = "fa-solid fa-heart btnOn bugFix";
+        } else {
+            heartIcon.className = "fa-regular fa-heart btnOff bugFix";
+        }
+    })
+
+    trashIcon.addEventListener('click', () => {
+        trashIcon.parentElement.remove();
+    })
 };
-
-
-
-heart.addEventListener('click', () => heartOnOff() );
 
 reviewInput.addEventListener('keyup', (e) => {
     if(reviewInput.value && e.key == 'Enter') {
