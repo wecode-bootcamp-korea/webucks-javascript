5173c5d34ee1535d6a00e54a56f71353d0644fb7
yeonjoo7 <jenny.likes.breeze@gmail.com>
2022-02-26 00:45:00 +0900

    ADD: detail 페이지 레이아웃, 기능 추가     - flex로 레이아웃 구성     - 좋아요 기능만 추가(클릭시 테두리만 빨간 하트)     - 리뷰 입력시 미리 설정한 아이디로만 리뷰 기능 추가

--- 4th/yeonjookim/js/detail.js
+++ 4th/yeonjookim/js/detail.js
@@ -1,11 +1,30 @@
-const likes = document.querySelector('#coffeeDetails__like')
+const likes = document.querySelector('.fas')
+
+const review = document.querySelector('.review')
+const reviewInput = document.querySelector('.review__input')
+
 
 function likethis() {
-    if (likes.innerText == "🤍") {
-        likes.innerText = "💝"
-    } else {
-        likes.innerText = "🤍"
+    likes.classList.toggle('liked')
+}
+
+function addReview(e) {
+    if (e.key === 'Enter') {
+        const reviewChild = document.createElement('div')
+        review.appendChild(reviewChild)
+        const reviewId = document.createElement('span')
+        reviewId.setAttribute('class', 'review__id')
+        const reviewText = document.createElement('span')
+        reviewText.setAttribute('class', 'review__text')
+        reviewChild.appendChild(reviewId)
+        reviewChild.appendChild(reviewText)
+        reviewId.textContent = 'new_customer'
+        reviewText.textContent = reviewInput.value
+        reviewInput.value = ""
     }
 }
 
+
 likes.addEventListener('click', likethis)
+
+reviewInput.addEventListener('keyup', addReview)
